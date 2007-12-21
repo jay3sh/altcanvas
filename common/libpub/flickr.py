2894f4293d139feb0037f0b73c1d744af160b0f1
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2007-12-21 08:07:47 +0000

    Copyright notices.

--- common/libpub/flickr.py
+++ common/libpub/flickr.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+# Publishr to publish images on web
+# Copyright (C) 2007  Jayesh Salvi 
+# http://www.altcanvas.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 # Copyright (c) 2007 by the respective coders, see
 # http://flickrapi.sf.net/
 #
