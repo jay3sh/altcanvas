85026dba35be9fb978406b17375d85ae006dc5e4
jayeshsalvi@gmail.com <jayeshsalvi@gmail.com@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-03-30 01:44:07 +0000

    initial code for change listeners and event propogation and a new widget 'pad'

---
+++ common/libpub/prime/widgets/pad.py
@@ -0,0 +1,16 @@
+
+import cairo
+from libpub.prime.widget import Widget
+import libpub.prime.mask as mask
+
+class Pad(Widget):
+    surface = None
+    
+    def __init__(self,w,h):
+        Widget.__init__(self,w,h)
+        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,w,h)
+        ctx = cairo.Context(self.surface)
+        ctx.set_source_rgb(0,0,0)
+        lgrad = mask.Linear(w,h).surface
+        ctx.rectangle(0,0,w,h)
+        ctx.mask_surface(lgrad)
