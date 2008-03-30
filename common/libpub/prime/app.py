b1fbba5b35166c6551d3f2407ae6ca72e79ed8e2
jayeshsalvi@gmail.com <jayeshsalvi@gmail.com@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-03-30 17:57:40 +0000

    implementing overlapping of widgets

--- common/libpub/prime/app.py
+++ common/libpub/prime/app.py
@@ -10,6 +10,79 @@ from libpub.prime.utils import get_image_locations,get_uniform_fit, \
 FOLDER_PATH='/photos/altimages/jyro'
 #FOLDER_PATH='/mnt/bluebox/photos'
 
+
+class WidgetWrapper:
+    widget = None
+    x = -1
+    y = -1
+    isKeyActive = True
+    isPointerActive = True
+    
+    def __init__(self,widget,x,y,isKeyActive=True,isPointerActive=True):
+        self.widget = widget
+        self.x = x
+        self.y = y
+        self.isKeyActive = isKeyActive
+        self.isPointerActive = isPointerActive
+
+class WidgetQueue:
+    widgetQ = []
+    
+    def __init__(self):
+        pass
+    
+    def __recalculate_focus(self,newWidget):
+        '''
+            @summary: This method compares surface extents of all existing widgets
+                with the new widget and determines if the new widget will
+                overlap with them. If it does overlap the existing widgets will 
+                loose their isKeyActive and isPoinerActive statuses.
+            @param newWidget: New widget being appended to the widgetQ
+            
+            
+    (ox0,oy0)<-------- ow -------->
+           ^ ----------------------
+           | |                    |
+           | |                    |
+          oh |                    |
+           | |                    |
+           v ---------------------- (ox1,oy1)
+           
+           
+                        (nx0,ny0)<-------- nw -------->
+                               ^ ----------------------
+					           | |                    |
+					           | |                    |
+					          nh |     newWidget      |
+					           | |                    |
+					           v ---------------------- (nx1,ny1)
+        '''
+        for ww in self.widgetQ:
+           ox0 = ww.x
+           oy0 = ww.y
+           ox1 = ox0 + ww.widget.w
+           oy1 = oy0 + ww.widget.h
+           
+           nx0 = newWidget.x
+           ny0 = newWidget.y
+           nx1 = nx0 + newWidget.widget.w
+           ny1 = ny0 + newWidget.widget.h
+            
+           if (ox0 < nx0 and ox1 < nx0) or (ox0 > nx1 and ox1 > nx1) or \
+               (oy0 < ny0 and oy1 < ny0) or (oy0 > ny1 and  oy1 > ny1):
+               # There is no overlap
+               continue
+           
+           ww.isKeyActive = False
+           ww.isPointerActive = False
+    
+    def enqueue(self,widgetWrapper):
+        self.widgetQ.append(widgetWrapper)
+
+    def next(self):
+        for widget in self.widgetQ:
+            yield(widget)
+            
 class App:
     widgetQ = []
     
@@ -24,7 +97,11 @@ class App:
     # Publishr app specific private members
     images = []
     
+    inputPad = None
+    
     def __init__(self):                    
+        
+        self.widgetQ = WidgetQueue()
         
         # Setup cairo surface and context
         self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,
@@ -56,14 +133,14 @@ class App:
             img = Image(self.images[i],imgw,imgh, 
                         X_MARGIN=int(0.1*imgw),Y_MARGIN=int(0.1*imgh))
             img.register_click_listener(self.on_image_click)
-            self.widgetQ.append((img,x,y))
+            self.widgetQ.enqueue(WidgetWrapper(img,x,y))
             i = i+1
 
     def __update_surface(self):
         # Create Image widgets for images and lay them out on the surface
         #gradient = mask.MoonRise(imgw,imgh).surface
-        for widget in self.widgetQ:
-            self.ctx.set_source_surface(widget[0].surface,widget[1],widget[2])
+        for ww in self.widgetQ.next():
+            self.ctx.set_source_surface(ww.widget.surface,ww.x,ww.y)
             #self.ctx.mask_surface(gradient,x,y)
             self.ctx.paint()
             
@@ -80,16 +157,18 @@ class App:
         self.change_listener = change_listener
         
     def dispatch_key_event(self,keyval,state):
-        for widget in self.widgetQ:
-            if hasattr(widget[0],'key_listener'):
-                widget[0].key_listener(keyval,state)
+        for ww in self.widgetQ.next():
+            if hasattr(ww.widget,'key_listener'):
+                ww.widget.key_listener(keyval,state)
                 
     def dispatch_pointer_event(self,x,y,pressed):
-        for widget in self.widgetQ:
-            if hasattr(widget[0],'pointer_listener'):
-                widget[0].pointer_listener(x-widget[1],y-widget[2],pressed)
+        for ww in self.widgetQ.next():
+            if hasattr(ww.widget,'pointer_listener'):
+                ww.widget.pointer_listener(x-ww.x,y-ww.y,pressed)
                 
     def on_image_click(self,image):
-        pad = Pad(int(2*self.app_width/3),int(2*self.app_height/3))
-        self.widgetQ.append((pad,int(self.app_width/3)-20,int(self.app_height/3)-20))
-        self.__update_surface()
+        if not self.inputPad:
+            self.inputPad = Pad(int(2*self.app_width/3),int(2*self.app_height/3))
+            self.widgetQ.enqueue(WidgetWrapper(
+                self.inputPad,int(self.app_width/3)-20,int(self.app_height/3)-20))
+            self.__update_surface()
