6c449ede2ece3a919056ab972a73ff6c615bf083
jayeshsalvi@gmail.com <jayeshsalvi@gmail.com@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-03-31 06:09:20 +0000

    draw gradient inside the Image object.

--- common/libpub/prime/widgets/image.py
+++ common/libpub/prime/widgets/image.py
@@ -1,6 +1,7 @@
 import cairo
 import gtk
 from libpub.prime.widget import Widget
+import libpub.prime.mask as mask
 
 class Image(Widget):
     surface = None
@@ -9,19 +10,30 @@ class Image(Widget):
     
     def __init__(self,path,w,h,X_MARGIN=0,Y_MARGIN=0):
         Widget.__init__(self, w, h)
-        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,w,h)
-        ctx = cairo.Context(self.surface)
-        ctx.set_source_rgb(1,1,1)
-        ctx.rectangle(0,0,w,h)
-        ctx.fill()
+        surface1 = cairo.ImageSurface(cairo.FORMAT_ARGB32,w,h)
+        ctx1 = cairo.Context(surface1)
+        
+        ctx2 = gtk.gdk.CairoContext(ctx1)
         
         self.path = path
-        ctx2 = gtk.gdk.CairoContext(ctx)
         pixbuf = gtk.gdk.pixbuf_new_from_file(path)
         scaled_pixbuf = pixbuf.scale_simple(
                             w-2*X_MARGIN,h-2*Y_MARGIN,gtk.gdk.INTERP_NEAREST)
+        ctx2.set_source_rgb(1,1,1)
+        ctx2.rectangle(0,0,w,h)
+        ctx2.fill()
         ctx2.set_source_pixbuf(scaled_pixbuf,X_MARGIN,Y_MARGIN)
         ctx2.paint()
+        
+        # Draw this on a third surface with a gradient
+        gradient = mask.MoonRise(w,h).surface
+        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,w,h)
+        ctx3 = cairo.Context(self.surface)
+        ctx3.set_source_rgb(0,0,0)
+        ctx3.rectangle(0,0,w,h)
+        ctx3.fill()
+        ctx3.set_source_surface(surface1)
+        ctx3.mask_surface(gradient)
         
     def register_click_listener(self,click_listener):
         self.click_listener = click_listener
