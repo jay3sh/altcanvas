2b2651b0d8f53130f9806f44ed8785bdb17e5aeb
jayeshsalvi@gmail.com <jayeshsalvi@gmail.com@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-04-07 02:57:31 +0000

    class based.

--- tests/thread-test.py
+++ tests/thread-test.py
@@ -5,27 +5,25 @@ import time
 
 (UNBLOCKED,BLOCKED) = range(2)
 
-i = UNBLOCKED
-
-def second_thread(sleeptime):
-    global i
-    i = BLOCKED
-    
-    time.sleep(sleeptime)
-    print 'Unblocking'
-    
-    i = UNBLOCKED
-    
-    
-def main():
-    global i
-    print i
-    thread.start_new_thread(second_thread, (5,))
-    
-    while True:
-        print i
-        time.sleep(1)
+class App:
+    input = UNBLOCKED
+    def second_thread(self):
+        self.input = BLOCKED
+        
+        time.sleep(5)
+        print 'Unblocking'
+        
+        self.input = UNBLOCKED
+        
+        
+    def run(self):
+        print self.input
+        thread.start_new_thread(self.second_thread, ())
+        
+        while True:
+            print self.input 
+            time.sleep(1)
         
 if __name__ == '__main__':
-    main()
+    App().run()
     
