c1ead0e9e3a0c89c5ce202627a40ab2a458bb442
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-05-11 23:53:43 +0000

    working animation on X

--- altX/animX.c
+++ altX/animX.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 #include <cairo.h>
 #include <cairo-xlib.h>
+
+#define DECLARE_P(type,var) \
+            type* var = NULL;
 
 #define ASSERT(x) \
         if (!(x)) { \
@@ -55,41 +60,60 @@ int main(int argc, char *argv[])
             break;
     }
 
-    printf("Filling!\n");
-
     /*
      * CAIRO Drawing
      */
-    cairo_surface_t *surface;
-    cairo_t *cr;
+    DECLARE_P(cairo_surface_t,surface);
+    DECLARE_P(cairo_t,ctx);
     Visual *visual = DefaultVisual(dpy,DefaultScreen(dpy));
+    ASSERT(visual)
 
     XClearWindow(dpy, win);
-    surface = cairo_xlib_surface_create(dpy, win, visual, w,h);
-    cr = cairo_create(surface);
+    ASSERT(surface =
+        cairo_xlib_surface_create(dpy, win, visual, w,h));
+    ASSERT(ctx =
+        cairo_create(surface));
 
-    cairo_set_source_rgb(cr, 0.8, 0.8, 0.8);
+
+    // Create widget surface
+    DECLARE_P(cairo_surface_t,wsurface)
+    DECLARE_P(cairo_t,wctx)
 
     #define SIZE 20
-    cairo_rectangle(cr,10,10,SIZE,SIZE);
-    cairo_fill(cr);
+    ASSERT(wsurface = cairo_image_surface_create(
+                            CAIRO_FORMAT_ARGB32,
+                            SIZE, SIZE));
 
-    if (cairo_status (cr)) {
+    ASSERT(wctx = cairo_create(wsurface));
+
+    cairo_set_source_rgb(wctx, 0.8, 0.8, 0.8);
+    cairo_rectangle(wctx,10,10,SIZE,SIZE);
+    cairo_fill(wctx);
+
+    if (cairo_status(wctx)) {
         printf("Cairo is unhappy: %s\n",
-            cairo_status_to_string (cairo_status (cr)));
+            cairo_status_to_string (cairo_status (wctx)));
         exit(0);
     }
 
-    cairo_destroy(cr);
-    cairo_surface_destroy(surface);
+    int i=0;
+    for (; i<200; i++)
+    {
+        XClearWindow(dpy, win);
+        cairo_set_source_surface(ctx,wsurface,10+i,10+i);
+        cairo_paint(ctx);
+        XFlush(dpy);
+        usleep(10*1000);
+    }
 
 
     XFlush(dpy);
 
-    sleep(10);
 
-    printf("Done!\n");
-
+    cairo_destroy(wctx);
+    cairo_surface_destroy(wsurface);
+    cairo_destroy(ctx);
+    cairo_surface_destroy(surface);
     XCloseDisplay(dpy);
 
     return 0;
