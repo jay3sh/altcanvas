79dfa8571e965e6f51a5f93895dc770d25c6ba29
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-05-22 08:13:50 +0000

    X addition

--- altX/canvasX.c
+++ altX/canvasX.c
@@ -1,10 +1,56 @@
 #include <Python.h>
-
 #include <stdio.h>
+
+#include <X11/Xlib.h>
+
+#define ASSERT(x) \
+        if (!(x)) { \
+           printf("Assertion failed: %s:%d << %s >>\n", \
+                __FILE__,__LINE__,__FUNCTION__); \
+           exit(1); \
+        }
+
+static Window win,rwin;
+static Display *dpy=NULL;
+static int screen=0;
+static int w=800;
+static int h=480;
+static int X=0;
+static int Y=0;
+static Pixmap pix;
+static XGCValues gcv;
+static GC gc;
 
 static PyObject *canvas_run(PyObject *self,PyObject *pArgs)
 {
-    printf("Canvas RUN\n");
+
+    dpy = XOpenDisplay(0);
+    ASSERT(dpy);
+
+    rwin = DefaultRootWindow(dpy);
+    ASSERT(rwin);
+
+    screen = DefaultScreen(dpy);
+    ASSERT(screen >= 0);
+
+    win = XCreateSimpleWindow(
+                dpy,
+                rwin,
+                X, Y,
+                w, h,
+                0,
+                BlackPixel(dpy,screen),
+                BlackPixel(dpy,screen));
+    ASSERT(win);
+
+    pix = XCreatePixmap(dpy,
+                        win,
+                        w,h,
+                        DefaultDepth(dpy,screen));
+    ASSERT(pix);
+    /*
+     * Return None
+     */
     Py_INCREF(Py_None);
     return Py_None;
 }
@@ -12,7 +58,7 @@ static PyObject *canvas_run(PyObject *self,PyObject *pArgs)
 static PyMethodDef canvas_methods[] =
 {
     { "run",canvas_run,METH_VARARGS,NULL},
-    { NULL, NULL }
+    { NULL, NULL, 0, NULL }
 };
 
 DL_EXPORT(void)
