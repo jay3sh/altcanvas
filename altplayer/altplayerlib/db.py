17e1d0b8ccaad3494c74886571e2faa5b024c299
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-06-09 06:58:23 +0000

    __setattr__ hacks

--- altplayer/altplayerlib/db.py
+++ altplayer/altplayerlib/db.py
@@ -4,21 +4,21 @@ import sqlite3
 
 
 class Record:
-    map = None
+    __map__ = {}
     def __init__(self,**kwds):
-        self.map = kwds
+        self.__dict__['__map__'] = kwds
 
     def __getattr__(self,elem):
         if elem is 'fieldnames':
-            return self.map.keys()
+            return self.__map__.keys()
         if elem is 'fieldvalues':
-            return self.map.values()
+            return self.__map__.values()
         if elem is 'fieldtypes':
-            return map(lambda x: type(x), self.map.values())
+            return __map__(lambda x: type(x), self.__map__.values())
         if elem is 'fields':
-            return self.map.items()
-        if elem in self.map.keys():
-            return self.map[elem]
+            return self.__map__.items()
+        if elem in self.__map__.keys():
+            return self.__map__[elem]
 
         raise AttributeError("Invalid Attribute '%s'"%elem)
 
@@ -26,7 +26,11 @@ class Record:
         if type(elem) != type('string'):
             raise AttributeError("Attribute name has to be a string")
 
-        self.map[elem] = value
+        if self.__dict__.has_key(elem):
+            self.__dict__[elem] = value
+            return
+
+        self.__map__[elem] = value
 
 
 
@@ -47,6 +51,12 @@ class DB:
         self.conn = sqlite3.connect(self.path)
         self.cur = self.conn.cursor()
 
+    def __quote_strings__(self,s):
+        if type(s) == type('string'):
+            return '"'+s+'"'
+        else:
+            return s
+
     def put(self,record):
         self.cur.execute("select name from sqlite_master where type='table'")
         tables = map(lambda x: x[0],self.cur.fetchall())
@@ -66,13 +76,8 @@ class DB:
             self.cur.execute(sql)
             self.conn.commit()
 
-        def quote_strings(s):
-            if type(s) == type('string'):
-                return '"'+s+'"'
-            else:
-                return s
 
-        fieldvalues = map(quote_strings,record.fieldvalues)
+        fieldvalues = map(self.__quote_strings__,record.fieldvalues)
 
         # Now insert the record fields into the database
         sql = 'insert into %s'%tablename
@@ -84,6 +89,7 @@ class DB:
         sql += reduce(lambda x,y: '%s,%s'%(x,y), fieldvalues)
         sql += ')'
 
+        return None
         self.cur.execute(sql)
         self.conn.commit()
 
@@ -98,6 +104,14 @@ class DB:
         fields = map(lambda x:x[1], self.cur.fetchall())
         fields = filter(lambda x: x != 'ID',fields)
 
+        sql = 'select '
+        sql += reduce(lambda x,y: '%s,%s'%(x,y), fields)
+        sql += ' from '+tablename
+        sql += ' where '
+
+        fieldvalues = map(self.__quote_strings__,record.fieldvalues)
+        print fieldvalues 
+
         return None
         key_str = None
 
@@ -110,7 +124,6 @@ class DB:
 
         sql = "select filename,image_url,image_path from coverart where "+key_str
 
-        print sql
 
         self.cur.execute(sql)
 
@@ -131,7 +144,7 @@ if __name__ == '__main__':
             'image_url':'http://aws.amazon.com/something.jpg',
             'image_path':'/home34343434/.coverart/something.jpg'})
     '''
-    print db.get(CoverartRecord(filename='isobel.mp3'))
+    print db.get(CoverartRecord(filename=' isobel.mp3'))
 
     '''
     r = CoverartRecord(filename=' isobel.mp3',
