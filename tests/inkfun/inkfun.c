99fe42dbd165301272d231b79cbed08a822e10f7
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-07-06 22:53:47 +0000

    progress.

--- tests/inkfun/inkfun.c
+++ tests/inkfun/inkfun.c
@@ -6,6 +6,11 @@
  * gcc -I$HOME/include -I/usr/include/libxml2 inkfun.c \
  *      -L/usr/lib64 -lxml2 -o inkfun
  *
+ * Valgrind instructions:
+ * G_SLICE=always-malloc G_DEBUG=gc-friendly valgrind \
+ * --log-file-exactly=inkfun.valgrind \
+ * --leak-check=full --leak-resolution=high
+ *
  */
 
 #include <boiler.h>
@@ -24,14 +29,16 @@
 /*
  * @class inkObject
  */
+struct _inkObject_t;
+
+typedef struct _inkObject_t inkObject_t;
 
 struct _inkObject_t{
-    xmlChar *id;
+    char *id;
     unsigned int width;
     unsigned int height;
-    struct _inkObject_t *next;
+    inkObject_t *next;
 };
-typedef struct _inkObject_t inkObject_t;
 
 inkObject_t *new_inkObject()
 {
@@ -56,8 +63,11 @@ void delete_inkObject(inkObject_t *p)
  * @class inkGui
  */
 
-#define XMLSTR_EQUALS(x,y) \
+#define XML_EQUALS(x,y) \
     !(xmlStrcmp((const xmlChar *)x,(const xmlChar *)y))
+
+#define XML_GETATTR(reader,key) \
+    xmlTextReaderGetAttribute(reader,(const xmlChar *)key)
 
 struct _inkGui_t{
     unsigned int width;
@@ -80,23 +90,46 @@ inkGui_t *new_inkGui(const char *svgfilename)
     int ret = 0;
     xmlChar *nodeName = NULL;
     xmlChar *attr = NULL;
+    xmlChar *className = NULL;
+
+    /*
+     * Create Objects to fill in the parsed SVG data
+     */
+    inkGui_t *inkGui = NULL;
+    ASSERT(inkGui= (inkGui_t *)malloc(sizeof(inkGui_t)))
+    memset(inkGui,0,sizeof(inkGui_t));
 
     while((ret = xmlTextReaderRead(reader)) == 1)
     {
         ASSERT(nodeName = xmlTextReaderLocalName(reader))
-        if(XMLSTR_EQUALS(nodeName,"svg"))
+        if(XML_EQUALS(nodeName,"svg"))
         {
-            attr = xmlTextReaderGetAttribute(reader,(const xmlChar *)"width");
-            printf("width %s\n",attr);
+            attr = XML_GETATTR(reader,"width");
+            inkGui->width = atoi((char *)attr);
             xmlFree(attr);
-            attr = xmlTextReaderGetAttribute(reader,(const xmlChar *)"height");
-            printf("height %s\n",attr);
+            attr = XML_GETATTR(reader,"height");
+            inkGui->height = atoi((char *)attr);
             xmlFree(attr);
         }
         xmlFree(nodeName);
 
-    }
+        /*
+         * Check if this node is an inkObject element
+         * i.e. if it has a class attribute
+         */
+        if((className = XML_GETATTR(reader,"class")))
+        {
+            inkObject_t *inkObject = NULL;
+            ASSERT(inkObject = new_inkObject())
+            inkObject->id = strndup((char *)className,1024);
+            xmlFree(className);
 
+            /* link the new object in inkGui's inkObject list */
+            inkObject->next = inkGui->inkObjectList;
+            inkGui->inkObjectList = inkObject;
+        }
+
+    }
 
     xmlFreeTextReader(reader);
     ASSERT(ret == 0)
@@ -105,15 +138,22 @@ inkGui_t *new_inkGui(const char *svgfilename)
     /* 
      * Create objects out of parsed SVG XML 
      */
-    inkGui_t *p = NULL;
-    ASSERT(p= (inkGui_t *)malloc(sizeof(inkGui_t)))
 
-    return p;
+    return inkGui;
 }
 
 void delete_inkGui(inkGui_t *inkGui)
 {
+    inkObject_t *inkObject;
+    inkObject_t *tmp;
+
     if(inkGui){
+        inkObject = inkGui->inkObjectList;
+        while(inkObject) {
+            tmp = inkObject->next;
+            delete_inkObject(inkObject);
+            inkObject = tmp;
+        }
         //TODO: cleanup inkObject list
         free(inkGui);
     }
@@ -121,7 +161,18 @@ void delete_inkGui(inkGui_t *inkGui)
 
 BEGIN_MAIN(1,"inkfun <filename>")
 
-    new_inkGui(argv[1]);
+    inkGui_t *inkGui = NULL;
+    inkObject_t *inkObject = NULL;
+    ASSERT(inkGui = new_inkGui(argv[1]))
+
+    inkObject = inkGui->inkObjectList;
+    while(inkObject)
+    {
+        printf("inkObject = %s\n",inkObject->id);
+        inkObject = inkObject->next;
+    }
+
+    delete_inkGui(inkGui);
 
 #if 0
     xmlDoc *doc = NULL;
