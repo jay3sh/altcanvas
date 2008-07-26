02203b8558844f93c7680e6e58084bab7da21f51
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-07-26 23:09:20 +0000

    POC working.

--- inkface/x11-rsvg.c
+++ inkface/x11-rsvg.c
@@ -1,9 +1,23 @@
 #include "rsvg.h"
+#include "rsvg-cairo.h"
+#include "rsvg-cairo-draw.h"
+#include "rsvg-cairo-render.h"
+#include "rsvg-styles.h"
+#include "rsvg-structure.h"
+
+struct _RsvgNodeRect {
+    RsvgNode super;
+    RsvgLength x, y, w, h, rx, ry;
+    gboolean got_rx, got_ry;
+};
+
+typedef struct _RsvgNodeRect RsvgNodeRect;
 
 #include "stdlib.h"
 
 #include <cairo.h>
 #include <cairo-xlib.h>
+
 
 #define ASSERT(x) \
         if (!(x)) { \
@@ -11,6 +25,52 @@
                 __FILE__,__LINE__,__FUNCTION__); \
            exit(1); \
         }
+
+/* Constructor and Destructor Prototypes */
+
+void main_constructor( void )
+__attribute__ ((no_instrument_function, constructor));
+
+void main_destructor( void )
+__attribute__ ((no_instrument_function, destructor));
+
+void __cyg_profile_func_enter( void *func_address, void *call_site )
+__attribute__ ((no_instrument_function));
+
+void __cyg_profile_func_exit ( void *func_address, void *call_site )
+__attribute__ ((no_instrument_function));
+
+
+/* Output trace file pointer */
+static FILE *fp;
+
+void main_constructor( void )
+{
+  fp = fopen( "trace.txt", "w" );
+    if (fp == NULL) exit(-1);
+}
+
+
+void main_deconstructor( void )
+{
+  fclose( fp );
+}
+
+void __cyg_profile_func_enter( void *this, void *callsite )
+{
+   /* Function Entry Address */
+   fprintf(fp, "E%p\n", (int *)this);
+}
+
+
+void __cyg_profile_func_exit( void *this, void *callsite )
+{
+   /* Function Exit Address */
+   fprintf(fp, "X%p\n", (int *)this);
+}
+
+
+
 
 RsvgHandle *
 rsvg_handle_from_file(const char *filename)
@@ -45,6 +105,25 @@ rsvg_handle_from_file(const char *filename)
     ASSERT(rsvg_handle_close(handle,NULL));
     
     return handle;
+}
+
+void
+get_rect_node_dimensions(
+    RsvgHandle *handle, const char *id,double *w, double *h)
+{
+    RsvgNode *childNode = NULL;
+
+    ASSERT(id)    
+    ASSERT(handle)    
+    ASSERT(childNode = rsvg_defs_lookup (handle->priv->defs, id))
+
+    RsvgNodeRect *rectNode = NULL;
+    rectNode = (RsvgNodeRect *)childNode;
+
+    *w = rectNode->w.length;
+    *h = rectNode->h.length;
+
+    return;
 }
 
 int main(int argc, char *argv[])
@@ -102,11 +181,26 @@ int main(int argc, char *argv[])
                         dpy, win, visual, dim.width,dim.height));
     ASSERT(ctx = cairo_create(surface));
 
+    /*
     if(argc == 3){
         rsvg_handle_render_cairo_sub(handle,ctx,argv[2]);
     } else {
         rsvg_handle_render_cairo(handle, ctx);
     }
+    */
+
+    double nw=0.,nh=0.;
+    cairo_surface_t *node_surface = NULL;
+    cairo_t *node_cr = NULL;
+    get_rect_node_dimensions(handle,argv[2],&nw,&nh);
+
+    printf("%s dims = %f x %f\n",argv[2],nw,nh);
+    ASSERT(node_surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, (int)nw, (int)nh))
+    ASSERT(node_cr = cairo_create(node_surface))
+    rsvg_handle_render_cairo_sub(handle,node_cr,argv[2]);
+
+    cairo_set_source_surface(ctx,node_surface,0,0);
+    cairo_paint(ctx);
 
     XFlush(dpy);
 
