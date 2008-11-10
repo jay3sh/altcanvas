d2b5aeed99578881c96d53a64e6a90a36d8dbc1d
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-11-10 02:37:19 +0000

    example of loading multiple SVG documents in single App.

--- inkface/apps/notes/notes.py
+++ inkface/apps/notes/notes.py
@@ -3,7 +3,32 @@
 import inkface
 
 NOTEPAD_SVG='notepad.svg'
+KEYBOARD_SVG='keyboard-black.svg'
 canvas = None
+
+
+class Keyboard:
+    def __init__(self,canvas):
+        self.canvas = canvas
+        self.elements = inkface.loadsvg(KEYBOARD_SVG)
+        for e in self.elements:
+            if e.name.startswith('key'):
+                e.onTap = self.onTap
+            elif e.name.endswith('Glow'):
+                e.opacity = 0
+                e.onDraw = self.glowDraw
+
+    def onTap(self,e,elements):
+        pass
+        
+    def glowDraw(self,e):
+        print 'glowDrawing'
+        if e.opacity:
+            self.canvas.draw(e)
+
+    def onKeyPress(self,txt,code,elements):
+        print txt
+
 
 def onKeyPress(note,txt,code,elements):
     global canvas
@@ -28,6 +53,11 @@ def main():
             e.text = ''
             e.onKeyPress = onKeyPress
             e.refresh()
+
+    # Temp
+    kbd = Keyboard(canvas)
+    canvas.register_elements(kbd.elements)
+    # /Temp
 
     canvas.eventloop()
 
