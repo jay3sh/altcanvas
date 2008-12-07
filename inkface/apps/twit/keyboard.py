e32db4402e547a7ef332859d5f6d12df17ad3999
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-12-07 05:34:16 +0000

    keyboard text update logic.

--- inkface/apps/twit/keyboard.py
+++ inkface/apps/twit/keyboard.py
@@ -1,6 +1,7 @@
 
 import inkface
 import inklib
+import re
 
 KEYBOARD_SVG='keyboard.svg'
 
@@ -8,19 +9,23 @@ class Keyboard(inklib.Face):
     visible = False
     glowing_elements = {}
     def __init__(self,canvas):
-        #self.canvas = canvas
-        #self.elements = inkface.loadsvg(KEYBOARD_SVG)
         inklib.Face.__init__(self,canvas,KEYBOARD_SVG)
 
         for k,e in self.elements.items():
             if e.name.startswith('key'):
-                e.onTap = self.onTap
                 if e.name.endswith('Glow'):
                     e.opacity = 0
                     e.onDraw = self.glowDraw
                     self.glowing_elements[e.name] = 0
+                else:
+                    e.onTap = self.onAlNumTap
 
+        self.keySpace.onTap = self.onSpace
         self.keyEnter.onTap = self.onEnter
+
+        self.keyboardText.text = ''
+        self.keyboardText.refresh()
+
         self.canvas.onTimer = self.onTimer
         self.canvas.timeout = 100
 
@@ -32,7 +37,15 @@ class Keyboard(inklib.Face):
                 self.elements[k].opacity = 0
                 self.canvas.refresh()
 
-    def onTap(self,e):
+    def onSpace(self,e):
+        self.keyboardText.text += ' '
+        self.keyboardText.refresh()
+
+    def onAlNumTap(self,e):
+        m = re.match('key(\w)',e.name)
+        letter = m.group(1)
+        self.keyboardText.text += letter
+        self.keyboardText.refresh()
         try:
             ge = self.elements[e.name+'Glow']
             if ge: 
@@ -40,6 +53,7 @@ class Keyboard(inklib.Face):
                 self.glowing_elements[ge.name] = 3
         except KeyError,ke:
             pass
+        self.canvas.refresh()
 
     def onEnter(self,e):
         self.canvas.remove(self)
@@ -50,7 +64,7 @@ class Keyboard(inklib.Face):
             self.canvas.draw(e)
 
     def onKeyPress(self,txt,code,elements):
-        print txt
+        pass
 
 
 
