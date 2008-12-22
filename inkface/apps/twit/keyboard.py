992b84564e604fe9c76f69626240a073ba4e260b
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2008-12-22 02:09:36 +0000

    making keyboard implementation more complete.

--- inkface/apps/twit/keyboard.py
+++ inkface/apps/twit/keyboard.py
@@ -5,14 +5,46 @@ import re
 
 KEYBOARD_SVG='keyboard.svg'
 
+special_key_map = {
+    'Tilde':'~',
+    'Period':'.',
+    'Comma':',',
+    'SemiColon':';',
+    'Question':'?',
+    'Exclaim':'!',
+    'At':'@',
+    'Pound':'#',
+    'Dollar':'$',
+    'Percent':'%',
+    'Star':'*',
+    'Asterisk':'*',
+    'SQuote':'\'',
+    'DQuote':'"',
+    'LPara':'(',
+    'RPara':')',
+    'LSqBracket':'[',
+    'RSqBracket':']',
+    'FSlash':'/',
+    'BSlash':'\\',
+    'Amp':'&',
+    'Caret':'^',
+    'Equal':'=',
+    'Minus':'-',
+    'Plus':'+'
+    }
+
+
 class Keyboard(inklib.Face):
-    visible = False
     glowing_elements = {}
+    hidetext = False
+    typedtext = ''
     def __init__(self,canvas):
         inklib.Face.__init__(self,canvas,KEYBOARD_SVG)
 
         for k,e in self.elements.items():
-            if e.name.startswith('key'):
+            if e.name.startswith('keySp') and not e.name.endswith('Glow'):
+                e.onTap = self.onSpecialKey;
+            elif e.name.startswith('key'):
                 if e.name.endswith('Glow'):
                     e.opacity = 0
                     e.onDraw = self.glowDraw
@@ -22,14 +54,47 @@ class Keyboard(inklib.Face):
 
         self.keySpace.onTap = self.onSpace
         self.keyEnter.onTap = self.onEnter
+        self.keyCancel.onTap = self.onCancel
+        self.keyBackspace.onTap = self.onBackspace
         self.keyboardText.onKeyPress = self.onKeyPress
 
+        self.keyboardText.onTap = None
+        self.keyboardEntry.onTap = None
         self.keyboardText.text = ''
         self.keyboardText.refresh()
 
-        self.canvas.onTimer = self.onTimer
-        self.canvas.timeout = 100
+        #self.canvas.onTimer = self.onTimer
+        #self.canvas.timeout = 100
 
+    def onSpecialKey(self,e):
+        m = re.match('keySp(\w+)',e.name)
+
+        if not m: return 
+
+        keyName = m.group(1)
+        try:
+            keyLetter = special_key_map[keyName] 
+        except KeyError,ke:
+            pass
+
+        if not keyLetter: return
+
+        if self.hidetext:
+            self.keyboardText.text += '*'
+            self.typedtext += keyLetter
+        else:
+            self.keyboardText.text += keyLetter
+            self.typedtext += keyLetter
+
+        self.keyboardText.refresh()
+        canvas.refresh()
+        
+    def onBackspace(self,e):
+        self.keyboardText.text = self.keyboardText.text[:-1]
+        self.typedtext = self.typedtext[:-1]
+        self.keyboardText.refresh()
+        self.canvas.refresh()
+        
     def onTimer(self):
         for k,v in self.glowing_elements.items():
             if v > 0:
@@ -45,22 +110,28 @@ class Keyboard(inklib.Face):
     def onAlNumTap(self,e):
         m = re.match('key(\w)',e.name)
         letter = m.group(1)
-        self.keyboardText.text += letter
+        if self.hidetext:
+            self.keyboardText.text += '*'
+            self.typedtext += letter
+        else:
+            self.keyboardText.text += letter
+            self.typedtext += letter
+
         self.keyboardText.refresh()
         try:
             ge = self.elements[e.name+'Glow']
             if ge: 
-                ge.opacity = 1
+                ge.opacity = 0
                 self.glowing_elements[ge.name] = 3
         except KeyError,ke:
             pass
         self.canvas.refresh()
 
     def onEnter(self,e):
-        self.resultProcessor(self.keyboardText.text)
+        self.resultProcessor(self.typedtext)
         
     def onCancel(self,e):
-        self.resultProcessor()
+        self.resultProcessor(None)
 
     def glowDraw(self,e):
         if e.opacity:
@@ -71,9 +142,26 @@ class Keyboard(inklib.Face):
         self.keyboardText.refresh()
 
     def onKeyPress(self,e,txt,keycode):
-        self.keyboardText.text += txt.strip()
+        
+        if self.hidetext:
+            self.keyboardText.text += '*'
+            self.typedtext += txt.strip()
+        else:
+            self.keyboardText.text += txt.strip()
+            self.typedtext += txt.strip()
+
+        # Special keys
         if keycode == inkface.KeyEscape:
             self.onCancel(e)
+        if keycode == inkface.KeyBackspace:
+            self.onBackspace(e)
+        elif keycode == inkface.KeySpace:
+            if self.hidetext:
+                self.keyboardText.text += '*'
+                self.typedtext += ' '
+            else:
+                self.keyboardText.text += ' '
+                self.typedtext += ' '
         elif keycode == inkface.KeyEnter:
             self.onEnter(e)
 
