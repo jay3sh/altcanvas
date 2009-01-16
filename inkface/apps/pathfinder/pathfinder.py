87da480e88314394d891c6a738c62520836ce254
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-01-16 06:59:52 +0000

    Add animation effect with hardcoded path.

--- inkface/apps/pathfinder/pathfinder.py
+++ inkface/apps/pathfinder/pathfinder.py
@@ -9,7 +9,11 @@ import clutter
 import cluttercairo
 import cairo
 
-
+############################################################################
+# @class Face 
+#   Encapsulation of an SVG file
+#   The elements of the SVG file are accessible as attributes of Face object 
+############################################################################
 class Face:
     def __init__(self,svgname,autoload=True):
         assert(os.path.exists(svgname))
@@ -23,12 +27,73 @@ class Face:
             del self.__dict__[name]
         inkface.unload(self.elements); 
 
+############################################################################
+# @class ClutterFace 
+#   Derivative of Face - creates Clutter Actors for each element of Face
+#   accessible as attributes by name <element_name>+'_actor'
+############################################################################
+class ClutterFace(Face):
+    def __init__(self,svgname):
+        Face.__init__(self,svgname=svgname)
 
-def main():
-    f = Face(svgname='paths.svg')
-    print f.path0.d
+        for name,elem in self.elements.items():
+            actor = cluttercairo.CairoTexture(width=elem.w,height=elem.h)
+            ctx = actor.cairo_create()
+            ctx.set_source_surface(elem.surface)
+            ctx.paint()
+            actor.set_position(elem.x,elem.y)
+            self.__dict__[name+'_actor'] = actor
+            del(ctx)
+
+
+
+def on_key_press_event (stage, event):
+    clutter.main_quit()
+
+
+
+class PathFinderApp:
+    def __init__(self):
+        self.face = ClutterFace(svgname='paths.svg')
+
+    def main(self):
+        stage = clutter.Stage()
+        stage.set_color(
+            clutter.Color(red=0xff, green=0xff, blue=0xff, alpha=0xff))
+        stage.set_size(width=800, height=480)
+        stage.connect('key-press-event', on_key_press_event)
+        stage.connect('destroy', clutter.main_quit)
+
+        timeline = clutter.Timeline(fps=60, duration=3000)
+        timeline.set_loop(True)
+        
+        knots = ( \
+            (   0,   0 ),   \
+            ( 300,   0 ),   \
+            ( 300, 300 ),   \
+            (   0, 300 ),   \
+        )
+
+        alpha = clutter.Alpha(timeline, clutter.sine_func)
+
+        p_behavior = clutter.BehaviourBspline(alpha=alpha,knots=knots)
+        p_behavior.append_knots((0,0))
+        p_behavior.apply(self.face.ball_actor)
+
+
+        self.face.ball_actor.set_position(0,0)
+        self.face.ball_actor.show()
+
+        stage.add(self.face.ball_actor)
+        stage.show()
+    
+        timeline.start()
+        clutter.main()
+    
+        return 0
+
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(PathFinderApp().main())
 
