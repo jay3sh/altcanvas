67a8ae2af0d498daf204ae8c41a42184866c0642
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-02-04 21:46:27 +0000

    SVGElement class that will hold methods for rendering.

--- altsvg/__init__.py
+++ altsvg/__init__.py
@@ -36,14 +36,58 @@ def search_group(elem,name):
 
         if res: return res
 
+def get_elements(root_g,name=None):
+    for e in root_g.getchildren():
+        if not name:
+            #yield e
+            yield SVGElement(e.get('id'),e)
+        else:
+            if e.attrib.get(TAG_INKSCAPE_LABEL) == name:
+                #yield e
+                yield SVGElement(e.get('id'),e)
+            else:
+                continue
+
+def render_rect(node):
+    print 'Drawing rect '+node.attrib.get('id')
+
+def render_path(node):
+    print 'Drawing path '+node.attrib.get('id')
+    
+node_renderer = \
+    {
+        TAG_RECT:render_rect,
+        TAG_PATH:render_path
+    }
+
+class SVGElement:
+    surface = None
+    def __init__(self,name,xmlnode):
+        self.name = name
+        self.xmlnode = xmlnode
+
+    #
+    # @return surface PyCairo surface with rendering of this SVG element
+    #
+    def get_surface(self):
+        if not self.surface:
+            self.render()
+
+        return self.surface
+
+    def render(self):
+        r = node_renderer.get(self.xmlnode.tag,None)
+        if r:
+            r(self.xmlnode)
+        else:
+            print self.xmlnode.tag
+        pass
 
 def parse(filename):
     tree = ElementTree()
     tree.parse(filename)
     root_g = tree.find(TAG_G)
-    elem = search_group(root_g,'gObject')
-    if elem:    
-        print elem.attrib['id']
-    else:
-        print 'no elem found'
+
+    for e in get_elements(root_g):
+        print e.get_surface()
 
