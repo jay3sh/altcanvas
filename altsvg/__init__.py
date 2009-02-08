c4a6806ef9356d53fa93717c0ab3ba9b188e0607
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-02-08 00:28:59 +0000

    Pylint blessings and license info update.

--- altsvg/__init__.py
+++ altsvg/__init__.py
@@ -1,150 +1,87 @@
-'''An alternative implementation of libaltsvg. This one is in python'''
+# This file is part of altcanvas.
+#
+# altcanvas is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# altcanvas is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with altcanvas.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-#---------------------- EXPERIMENTAL -------------------------------
+'''An alternative implementation of libaltsvg. This one is in python'''
 
 from xml.etree.ElementTree import ElementTree
 import xml.etree
-
 import re
 
-svg_ns = "{http://www.w3.org/2000/svg}"
-inkscape_ns = "{http://www.inkscape.org/namespaces/inkscape}"
+SVG_NS      = "{http://www.w3.org/2000/svg}"
+INKSCAPE_NS = "{http://www.inkscape.org/namespaces/inkscape}"
 
-TAG_SVG=svg_ns+'svg'
-TAG_G = svg_ns+'g'
-TAG_PATH = svg_ns+'path'
-TAG_RECT = svg_ns+'rect'
-TAG_INKSCAPE_LABEL = inkscape_ns+'label'
-
-xml.etree.ElementTree._namespace_map['http://www.w3.org/2000/svg']='svg'
-
-def search_elem(elem,name):
-    try:
-        if elem.attrib[TAG_INKSCAPE_LABEL] == name:
-            return elem
-    except KeyError, ke:
-        pass
-
-
-def search_group(elem,name):
-    try:
-        if elem.attrib[TAG_INKSCAPE_LABEL] == name:
-            return elem
-    except KeyError, ke:
-        pass
-
-    for e in elem.getchildren():
-        if e.tag == TAG_G:
-            res = search_group(e,name)
-        else:
-            res = search_elem(e,name)
-
-        if res: return res
-
-def get_elements(root_g,name=None):
-    for e in root_g.getchildren():
-        if not name:
-            #yield e
-            yield SVGElement(e.get('id'),e)
-        else:
-            if e.attrib.get(TAG_INKSCAPE_LABEL) == name:
-                #yield e
-                yield SVGElement(e.get('id'),e)
-            else:
-                continue
-
-class SVGElement:
-    surface = None
-    def __init__(self,name,xmlnode):
-        self.name = name
-        self.xmlnode = xmlnode
-
-    #
-    # @return surface PyCairo surface with rendering of this SVG element
-    #
-    def get_surface(self):
-        if not self.surface:
-            self.render()
-
-        return self.surface
-
-    def render(self):
-        r = node_renderer.get(self.xmlnode.tag,None)
-        if r:
-            r(self.xmlnode)
-        else:
-            print self.xmlnode.tag
-
-def parse(filename):
-    tree = ElementTree()
-    tree.parse(filename)
-    root_g = tree.find(TAG_G)
-
-    for e in get_elements(root_g):
-        print e.get_surface()
-
-class SVGParser:
-    def __init__(self,docname):
-        self.tree = ElementTree()
-        self.tree.parse(docname)
-
-        #Find the first "g" tag and treat it as root "g" node
-        root_g = self.tree.find(TAG_G)
-        for e in root_g.getchildren():
-            print e.attrib.get('id')
-
-#---------------------- FORMAL -------------------------------
+TAG_SVG             = SVG_NS+'svg'
+TAG_G               = SVG_NS+'g'
+TAG_PATH            = SVG_NS+'path'
+TAG_RECT            = SVG_NS+'rect'
+TAG_INKSCAPE_LABEL  = INKSCAPE_NS+'label'
 
 def load_style(style_str):
+    ''' Parse and load style from style string '''
     style = {}
     for style_attr in style_str.strip().split(';'):
-        name,value = style_attr.split(':')
+        name, value = style_attr.split(':')
         style[name] = value
     return style
 
-def apply_style(cr,style):
+def apply_style(ctx, style):
+    ''' Modify given contex according to the style '''
     if style.has_key('stroke-width'):
-        cr.set_line_width(float(style['stroke-width']))
+        ctx.set_line_width(float(style['stroke-width']))
     if style.has_key('stroke-miterlimit'):
-        cr.set_miter_limit(float(style['stroke-miterlimit']))
+        ctx.set_miter_limit(float(style['stroke-miterlimit']))
 
-def render_rect(cr,node):
-
-    cr.save()
+def render_rect(ctx, node):
+    ''' Render 'rect' SVG element '''
+    ctx.save()
 
     style_str = node.attrib.get('style')
     if style_str:
         style = load_style(style_str)
         if style:
-            apply_style(cr,style)
+            apply_style(ctx, style)
 
     x = float(node.attrib.get('x'))
     y = float(node.attrib.get('y'))
     w = float(node.attrib.get('width'))
     h = float(node.attrib.get('height'))
-    cr.rel_move_to(x,y)
-    cr.rel_line_to(w,0)
-    cr.rel_line_to(0,h)
-    cr.rel_line_to(-w,0)
-    cr.rel_line_to(0,-h)
-    cr.stroke_preserve()
-    cr.rel_move_to(-x,-y)
+    ctx.rel_move_to(x, y)
+    ctx.rel_line_to(w, 0)
+    ctx.rel_line_to(0, h)
+    ctx.rel_line_to(-w, 0)
+    ctx.rel_line_to(0, -h)
+    ctx.stroke_preserve()
+    ctx.rel_move_to(-x, -y)
 
-    cr.restore()
+    ctx.restore()
 
-def render_path(cr,node):
-    cr.save()
+def render_path(ctx, node):
+    ''' Render 'path' SVG element '''
+    ctx.save()
 
     style_str = node.attrib.get('style')
     if style_str:
         style = load_style(style_str)
         if style:
-            apply_style(cr,style)
+            apply_style(ctx, style)
 
     pathdata = node.attrib.get('d')
     pathdata = pathdata.replace(',',' ')
 
-    dx,dy = cr.get_current_point()
+    dx, dy = ctx.get_current_point()
 
     tokens = pathdata.split()
     i = 0
@@ -154,13 +91,13 @@ def render_path(cr,node):
                 x = int(float(tokens[i+1]))
                 y = int(float(tokens[i+2]))
                 i += 3
-                cr.line_to(x+dx,y+dy)
+                ctx.line_to(x+dx, y+dy)
                 continue
             elif tokens[i] == 'M':
                 x = int(float(tokens[i+1]))
                 y = int(float(tokens[i+2]))
                 i += 3
-                cr.move_to(x+dx,y+dy)
+                ctx.move_to(x+dx, y+dy)
                 continue
             elif tokens[i] == 'C':
                 x1 = int(float(tokens[i+1]))
@@ -170,7 +107,7 @@ def render_path(cr,node):
                 x = int(float(tokens[i+5]))
                 y = int(float(tokens[i+6]))
                 i += 7
-                cr.curve_to(x1+dx,y1+dy,x2+dx,y2+dy,x+dx,y+dy)
+                ctx.curve_to(x1+dx, y1+dy, x2+dx, y2+dy, x+dx, y+dy)
                 continue
             elif tokens[i] == 'z':
                 break
@@ -180,11 +117,11 @@ def render_path(cr,node):
         # getting stuck in an infinite loop
         i += 1
 
-    cr.stroke_preserve()
-    cr.restore()
+    ctx.stroke_preserve()
+    ctx.restore()
 
     
-node_renderer = \
+NODE_RENDERER = \
     {
         TAG_RECT:render_rect,
         TAG_PATH:render_path
@@ -195,65 +132,65 @@ node_renderer = \
 # INTERFACE
 #
 
-#
-# @return tree
-#
 def load(svgname):
+    ''' load and parse SVG document, create ElementTree from the same '''
     tree = ElementTree()
     tree.parse(svgname)
     return tree
 
-#
-# @return extract properties of the doc - returns a simple tuple for now
-#
 def extract_doc_data(tree):
+    ''' extract properties of the doc - returns a simple tuple for now '''
     root = tree.getroot()
     try: 
         return(float(root.attrib.get('width')),
             float(root.attrib.get('height')))
-    except ValueError,ve:
-        print 'Error extracting SVG doc dimensions, falling back to 300x300'
-        return ((300,300))
+    except ValueError, ve:
+        print 'Error extracting SVG doc dimensions "%s", \
+            falling back to 300x300' % str(ve)
+        return ((300, 300))
 
 #
 # @return elementlist
 #
 def extract_svg_elements(tree):
+    ''' 
+    find SVG elements with INKSCAPE_LABEL set and export them as SVGElement 
+    objects 
+    '''
     root_g = tree.find(TAG_G)
     for e in root_g.getchildren():
         if e.attrib.has_key(TAG_INKSCAPE_LABEL):
             # Create SVG element for this node
             print e.attrib.get('id')
 
-def render_full(cr,tree):
+def render_full(ctx, tree):
+    ''' render the full SVG tree '''
     root_g = tree.find(TAG_G)
-    cr.move_to(0,0)
-    render(cr,root_g)
+    ctx.move_to(0, 0)
+    render(ctx, root_g)
         
-def walk(tree):
-    pass
-
-def render(cr,node):
+def render(ctx, node):
+    ''' render individual SVG node '''
     for e in node.getchildren():
         dx = 0
         dy = 0
         transform = e.attrib.get('transform')
         if transform:
             pattern = '(\w+)\(([0-9-.]+),([0-9-.]+)\)'
-            m = re.search(pattern,transform)
+            m = re.search(pattern, transform)
             if m: 
                 dx = float(m.group(2))
                 dy = float(m.group(3))
 
-        cr.rel_move_to(int(dx),int(dy))
+        ctx.rel_move_to(int(dx), int(dy))
 
         if e.tag == TAG_G:
-            render(cr,e)
+            render(ctx, e)
         else:
-            r = node_renderer.get(e.tag,None)
+            r = NODE_RENDERER.get(e.tag, None)
             if r:
-                r(cr,e)
+                r(ctx, e)
             else:
                 print e.tag
 
-        cr.rel_move_to(-int(dx),-int(dy))
+        ctx.rel_move_to(-int(dx), -int(dy))
