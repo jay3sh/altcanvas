274e77f001b5c75bf97f1d020c5b646f41f32158
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-02-14 13:23:40 +0000

    Visual slideshow feature implemented.

--- altsvg/tests/regression.py
+++ altsvg/tests/regression.py
@@ -2,8 +2,11 @@
 
 import sys
 import md5
+import pygame
 import cairo
+import numpy
 import altsvg
+from copy import copy
 
 def usage():
     print 'regression.py [options]'
@@ -13,10 +16,83 @@ def usage():
     print '             with the one freshly created from SVG test files'
 
 
+class SlideShow:
+    def rgb_voodo(self,surface):
+        buf = surface.get_data()
+        a = numpy.frombuffer(buf,numpy.uint8)
+        a.shape = (surface.get_width(),surface.get_height(),4)
+        tmp = copy(a[:,:,0])
+        a[:,:,0] = a[:,:,2]
+        a[:,:,2] = tmp
+        return a
+ 
+    def load_fresh_image(self,svgname):
+        self.vectorDoc = altsvg.VectorDoc(svgname)
+        self.iw,self.ih = map(lambda x: int(x), self.vectorDoc.get_doc_props())
+        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32,self.iw,self.ih)
+        ctx = cairo.Context(surface)
+        self.vectorDoc.render_full(ctx)
+
+        ctx.move_to(5,10)
+        ctx.show_text(svgname+" [FRESH]")
+        return surface
+ 
+    def load_saved_image(self,pngname):
+        surface = cairo.ImageSurface.create_from_png(pngname)
+        ctx = cairo.Context(surface)
+        ctx.move_to(5,10)
+        ctx.show_text(pngname+" [SAVED]")
+        return surface
+        
+    def main(self,svgfiles):
+        self.svgfiles = svgfiles
+        svgname = svgfiles[0]
+        pygame.init()
+        w, h = (840,520)
+        self.window = pygame.display.set_mode((w,h),pygame.DOUBLEBUF )
+        self.screen = pygame.display.get_surface()
+
+        #surface = self.load_fresh_image(svgname)
+        #surface = self.load_saved_image(svgname.replace('svg','png'))
+
+        self.counter = 0
+
+        #self.refresh_slide(surface)
+
+        while True: 
+            self.input(pygame.event.wait())
+
+    def refresh_slide(self, surface):
+        buf = self.rgb_voodo(surface)
+
+        image = pygame.image.frombuffer(buf.tostring(),
+                    (surface.get_width(),surface.get_height()),"RGBA")
+        self.screen.blit(image, (20,20))
+        pygame.display.flip() 
+
+       
+    def input(self,event):
+        if event.type == pygame.QUIT:
+            sys.exit(0) 
+        elif event.type == pygame.KEYDOWN:
+            index = self.counter/2
+            if(index < len(self.svgfiles)):
+                if(self.counter%2 == 0):
+                    surface = self.load_saved_image(
+                            self.svgfiles[index].replace('svg','png'))
+                else:
+                    surface = self.load_fresh_image(self.svgfiles[index])
+                    
+                self.refresh_slide(surface)
+                self.counter += 1
+            else:
+                sys.exit(0)
+
+
 def main():
     op = sys.argv[1]
 
-    SVG_FILES = map(lambda x: 'data/shape-%d.svg'%x, range(8))
+    SVG_FILES = map(lambda x: 'data/shape-%d.svg'%x, range(7))
     SVG_FILES += (map(lambda x: 'data/composite-%d.svg'%x, range(1)))
 
     if op == 'genpng':
@@ -28,6 +104,7 @@ def main():
             ctx = cairo.Context(surface)
             vdoc.render_full(ctx)
 
+            print 'Saving PNG image for '+file
             surface.write_to_png(file.replace('svg','png'))
 
     elif op == 'cmppng':
@@ -51,10 +128,11 @@ def main():
             saved_md5.update(saved_buf)
 
             if fresh_md5.digest() == saved_md5.digest():
-                sys.stdout.write('... pass\n')
+                sys.stdout.write('... passed\n')
             else:
                 sys.stdout.write('... failed\n')
-
+    elif op == 'viscmp':
+        SlideShow().main(SVG_FILES)    
     else:
         usage()
         sys.exit(0)
