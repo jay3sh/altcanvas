670d7e0ffd307a970452644ba048b95edce80e59
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-02-20 07:59:42 +0000

    Preparatory changes.

--- inkface/canvas/__init__.py
+++ inkface/canvas/__init__.py
@@ -1,77 +1,60 @@
 
 import unittest
 
-class Element:
-    x = 0
-    y = 0
-    w = 0
-    h = 0
-    def __init__(self,x,y,w,h):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        elem.callback = None
+
+class Face:
+    def __init__(self,svgname):
+        from inkface.altsvg import VectorDoc
+        self.vDoc = VectorDoc(svgname)
+
+        self.elements = self.vDoc.get_elements()
+        
+
+class PygameFace(Face):
+    def __init__(self,svgname):
+        Face.__init__(self,svgname)
+
+        self.mutable_group = pygame.sprite.RenderPlain()
+        self.immutable_group = pygame.sprite.RenderPlain()
 
 class Canvas:
-    xruler = []
-    yruler = []
-
-    def __init__(self,(width,height)):
-        self.xruler = [[0,width]]
-
-    def reset_ruler(self):
-        self.xruler = [[0,width]]
-
-    def register_element(self, elem):
-        new_slot = (-1,-1)
-        lower_slot = -1
-        upper_slot = -1
-        for i in range(len(self.xruler)):
-            l,u = self.xruler[i]
-            if elem.x > u:
-                continue
-            else:
-                if lower_slot < 0:
-                    lower_slot = i
-
-            if elem.x+elem.w < u:
-                upper_slot = i
-            else:
-                continue
-            
-        print 'lower_slot = %d, upper slot = %d'%(lower_slot,upper_slot)
-        if lower_slot == upper_slot:
-            new_slot = [elem.x,elem.x+elem.w]
-            s_low = [self.xruler[lower_slot][0],elem.x-1]
-            s_high = [elem.x+elem.w+1,self.xruler[lower_slot][1]]
-
-            del self.xruler[lower_slot]
-            self.xruler.insert(lower_slot,s_high)
-            self.xruler.insert(lower_slot,new_slot)
-            self.xruler.insert(lower_slot,s_low)
-        else:
-            self.xruler[lower_slot][1] = elem.x - 1
-            self.xruler[upper_slot][0] = elem.x+elem.w + 1
-
-            for i in range(upper_slot-lower_slot-1):
-                del self.xruler[lower_slot+1]
-            
-            self.xruler.insert(lower_slot+1,[elem.x,elem.x+elem.w])
-
-        print self.xruler
-
-class TestBasic(unittest.TestCase):
-    def setUp(self):
-        self.canvas = Canvas((800,480))
+    def __init__(self):
         pass
 
-    def testOne(self):
-        e = Element(20,40,100,100)
-        self.canvas.register_element(e)
-        self.canvas.register_element(Element(30,60,100,100))
+    def __recalculate_clouds(self):
+        pass
 
-if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestBasic)
-    testResult = unittest.TextTestRunner(verbosity=0).run(suite)
+    def add(self, face):
+        pass
+
+    def remove(self, face):
+        pass
+
+class PygameCanvas(Canvas):
+    def __init__(self,
+                resolution = (640,480),
+                caption='Inkface App',
+                framerate=25):
+
+        Canvas.__init__(self)
+
+        pygame.init()
+        screen = pygame.display.set_mode(resolution)
+        self.dispsurf = pygame.Surface(screen.get_size())
+        pygame.display.set_caption(caption)
+        self.clock = pygame.time.clock()
+        self.framerate = framerate
+
+    def __painter_thread(self):
+        while True:
+            self.clock.tick(self.framerate)
+
+    def __handle_event(self,event):
+        pass
+
+    def eventloop(self):
+        while True:
+            self.__handle_event(pygame.event.wait())
+
+    
     
