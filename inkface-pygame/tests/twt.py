9ac4992f51f3d87555adc2511fc019962749a751
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-02-23 18:07:11 +0000

    Images also display, although immovable and not aligned.

--- inkface-pygame/tests/twt.py
+++ inkface-pygame/tests/twt.py
@@ -4,8 +4,8 @@ from twitter import twitter
 import os
 
 class App:
-    GAP = 10
-    MAX_TWT_NUM = 5
+    GAP = 7
+    MAX_TWT_NUM = 7
     index = MAX_TWT_NUM 
     FRAMERATE = 20
     roll = []
@@ -33,7 +33,9 @@ class App:
         self.face = PygameFace('data/gui-6.svg')
 
         self.base_x = self.face.twt0.x
+        self.base_img_x = self.face.img0.x
         self.base_y = self.face.twt0.y
+        self.base_img_y = self.face.img0.y
         self.base_w = self.face.twt0.svg.w
         self.base_h = self.face.twt0.svg.h
 
@@ -44,11 +46,20 @@ class App:
                             new_x = self.base_x,
                             new_y = self.base_y+\
                                 ((i+1)*(self.base_h + self.GAP)))
+            self.face.clone('img0','img'+str(i+1),
+                            new_x = self.base_img_x,
+                            new_y = self.base_img_y+\
+                                ((i+1)*(self.base_h + self.GAP)))
 
         for i in range(self.MAX_TWT_NUM + 1):
             elem = self.face.get('twt'+str(i))
+            eimg = self.face.get('img'+str(i))
             elem.onDraw = self.drawTwt
-            elem.svg.text = self.get_twt().text
+            twt = self.get_twt()
+            img = self.load_image(twt)
+
+            eimg.sprite.image.blit(img,(0,0))
+            elem.svg.text = twt.text
             elem.refresh(svg_reload=True)
             self.roll.append(elem)
 
@@ -96,11 +107,26 @@ class App:
             j += 1
         return new_text
 
+    def load_image(self,twt):
+        import urllib
+        import pygame.image
+        imgurl = twt.GetUser().profile_image_url
+        localfile = '/tmp/'+imgurl.split('/')[-1]
+        try:
+            urllib.urlretrieve(imgurl,localfile)
+        except UnicodeError, ue:
+            print 'Error fetching img URL'+str(ue)
+            return None 
+        image = pygame.image.load(localfile)
+        return image
+        
     def rollToNext(self):
         incoming = self.roll[self.index]
         incoming.onDraw = self.drawTwt
         incoming.y = self.base_x + self.MAX_TWT_NUM*self.moveStep
-        incoming.svg.text = self.get_twt().text
+        twt = self.get_twt()
+        #img = self.load_image(twt)
+        incoming.svg.text = twt.text
         incoming.refresh(svg_reload=True)
 
         self.index = (self.index + 1)%(self.MAX_TWT_NUM + 1)
