5f6518ab17f475c0c26773c368a15e4f38d159c4
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-03-07 06:13:40 +0000

    experiments with matrices.

--- inkface-pygame/inkface/altsvg/tests/pygame-viewer.py
+++ inkface-pygame/inkface/altsvg/tests/pygame-viewer.py
@@ -7,23 +7,55 @@ import math
 from copy import copy
 from inkface.altsvg import VectorDoc
 
+import math
+
 def draw(surface):
     cr = cairo.Context(surface)
 
-    cr.scale(2,2)
+    cr.set_source_rgb(1,0,0)
 
-    cr.set_source_rgb(1,1,1)
-    cr.rectangle(100,100,200,200)
+    ax = 200
+    ay = 200
+    #cr.rectangle(ax,ay,100,100)
+    cr.move_to(ax,ay)
+    cr.curve_to(ax+30,ay+30,ax+60,ay+30,ax+90,ay)
+    cr.curve_to(ax+60,ay-30,ax+30,ay-30,ax,ay)
+    
+
+    ex1,ey1,ex2,ey2 = cr.stroke_extents()
     print cr.stroke_extents()
     cr.stroke()
 
-    #cr.scale(2,2)
+    midx,midy = ((ex1+(ex2-ex1)/2),(ey1+(ey2-ey1)/2))
+    orig_vector = (ex1,ey1,midx,midy)
+    orig_vector_mag = math.sqrt((midx-ex1)*(midx-ex1)+(midy-ey1)*(midy-ey1))
+    orig_vector_ang = math.acos((midx-ex1)/orig_vector_mag)
 
-    #cr.rectangle(120,120,50,50)
-    #cr.move_to(120,120)
-    #cr.line_to(170,170)
+    print (orig_vector_mag,orig_vector_ang)
 
-    #cr.stroke()
+
+    mat = cairo.Matrix(0.7071068,-0.7071068,0.7071068,0.7071068,0,0)
+    cr.transform(mat)
+
+    mat.invert()
+
+    cmidx,cmidy = mat.transform_point(midx,midy)
+
+    dx = orig_vector_mag * math.cos(orig_vector_ang)
+    dy = orig_vector_mag * math.sin(orig_vector_ang)
+
+    cx = cmidx - dx
+    cy = cmidy - dy
+    
+    print (cx,cy)
+    
+    cr.set_source_rgb(1,1,1)
+
+    cr.move_to(cx,cy)
+    cr.curve_to(cx+30,cy+30,cx+60,cy+30,cx+90,cy)
+    cr.curve_to(cx+60,cy-30,cx+30,cy-30,cx,cy)
+    #cr.rectangle(cx,cy,100,100)
+    cr.stroke()
     
 
     '''
