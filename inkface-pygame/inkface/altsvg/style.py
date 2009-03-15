cf3d0c45c8e43b53ab55738e8dc9cc500f1528d9
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-03-15 19:05:01 +0000

    Takes care of rgb(0,0,0) format of color specification.

--- inkface-pygame/inkface/altsvg/style.py
+++ inkface-pygame/inkface/altsvg/style.py
@@ -80,10 +80,20 @@ class Style:
     def __html2rgb(self, html_color):
         ''' Converts HTML color code to normalized RGB values '''
         try:
-            r = int(html_color[1:3], 16)
-            g = int(html_color[3:5], 16)
-            b = int(html_color[5:7], 16)
-            return (r/256., g/256., b/256.)
+            html_color = html_color.replace(' ','')
+            if html_color.startswith('rgb'):
+                pattern = 'rgb\(([0-9,]+)\)'
+                m = re.search(pattern, html_color)
+                color_str = m.group(1)
+                colors = color_str.split(',')
+                r,g,b = map(lambda x: int(float(x)), colors)
+                return (r/256., g/256., b/256.)
+            else:
+                # Assume it's a 6 char hex encoded string
+                r = int(html_color[1:3], 16)
+                g = int(html_color[3:5], 16)
+                b = int(html_color[5:7], 16)
+                return (r/256., g/256., b/256.)
         except Exception, e:
             print 'Error parsing color code [%s]: %s'% (html_color, str(e))
             return (0, 0, 0)
@@ -240,7 +250,7 @@ class Gradient:
         # TODO: matrix-dup-code
         transform_type = None
         if transform_str is not None:
-            transform = transform.replace(' ','')
+            transform_str = transform_str.replace(' ','')
             pattern = '(\w+)\s*\(([e0-9-.,]+)\)'
             m = re.search(pattern, transform_str)
             if m: 
