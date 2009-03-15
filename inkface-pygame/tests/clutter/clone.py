0f58fb3e328762445c3a2a2fed0291b5bfe8364e
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-03-15 10:28:20 +0000

    forgot to add this one.

---
+++ inkface-pygame/tests/clutter/clone.py
@@ -0,0 +1,35 @@
+
+import sys
+import os
+import pygame
+from inkface.canvas import ClutterFace, ClutterCanvas
+
+class App:
+    def main(self):
+        self.face = ClutterFace(sys.argv[1])
+
+        self.canvas = ClutterCanvas(
+            (int(self.face.svg.width),int(self.face.svg.height)))
+
+        self.face.clone('newsFlash','newsFlash2',new_x=50,new_y=200)
+
+        self.face.newsFlash2.svg.text = 'Any colorful news?!'
+
+        print 'Updated text: '+self.face.newsFlash2.svg.text
+        self.face.newsFlash2.refresh(svg_reload=True)
+        
+        
+        self.canvas.add(self.face)
+
+        try:
+            self.canvas.eventloop()
+        except KeyboardInterrupt, ki:
+            sys.exit(0)
+
+App().main()
+
+
+
+
+
+
