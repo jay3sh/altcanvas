b43bc3168235b818043fcec418b282252efe201e
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-03-16 16:36:05 +0000

    A reasonable looking page formatting algorithm is implemented for inkreader.

--- inkface-pygame/apps/inkreader/code/inkreader.py
+++ inkface-pygame/apps/inkreader/code/inkreader.py
@@ -6,50 +6,87 @@ from inkface.canvas import PygameFace, PygameCanvas
 
 SVG_FILE='/home/jayesh/altcanvas/inkface-pygame/apps/inkreader/svg/inkreader.svg'
 
-def main():
-    try:
-        face = PygameFace(SVG_FILE)
-    
-        canvas = PygameCanvas(
-                    (int(float(face.svg.width)),
-                        int(float(face.svg.height))),
-                    framerate = 0)
-    
-        bookname = sys.argv[1]
-
-        bookf = open(bookname,'r')
-
-        MAX_LEN = 50 
-        txtlines = []
-        i = 0
-        for line in bookf:
-            while len(line) > 0:
-                if len(line) < MAX_LEN:
-                    txtlines.append(line)
-                    line = ''
+class App:
+    def get_word(self, fd):
+        pending_newline = False
+        for line in fd:
+            words = line.split(' ')
+            for word in words:
+                if pending_newline:
+                    pending_newline = False
+                    yield '\n'
                 else:
-                    txtlines.append(line[:MAX_LEN])
-                    line = line[MAX_LEN:]
-
-                if len(txtlines) > 20:
+                    if word.find('\n') >= 0:
+                        pending_newline = True
+                        word = word.replace('\n','')
+                    yield word
+            
+    
+    
+    def get_line(self, fd):
+        curline = ''
+        for word in self.get_word(fd):
+            if word == '\n':
+                yield curline
+                curline = ''
+            else:
+                if not self.length_check(curline, word):
+                    yield curline
+                    curline = word
+                else:
+                    curline += ' '+word
+            
+    def length_check(self, line, word):
+        self.line_elem.svg.text = line+' '+word
+        self.line_elem.refresh(svg_reload=True)
+        return (self.line_elem.svg.w < \
+            (self.pad_elem.svg.w - 3*self.margin))
+        
+    
+    def main(self):
+        try:
+            self.face = PygameFace(SVG_FILE)
+        
+            canvas = PygameCanvas(
+                        (int(float(self.face.svg.width)),
+                            int(float(self.face.svg.height))),
+                        framerate = 0)
+        
+            bookname = sys.argv[1]
+    
+            bookf = open(bookname,'r')
+    
+            self.face.clone('page','roughpage')
+            self.face.roughpage.hide()
+    
+            self.line_elem = self.face.roughpage
+            self.pad_elem = self.face.pad
+            self.margin = self.face.page.svg.x
+    
+            i = 0
+            page = ''
+            self.line_generator = self.get_line(bookf)
+            while True:
+                line = self.line_generator.next()
+                print line
+                page += line+'\n'
+                i += 1
+                if i > 25:
                     break
-                
-            if len(txtlines) > 20:
-                break
-
-        face.textobj.svg.text = '\n'.join(txtlines)
-        face.textobj.refresh(svg_reload=True)
-
-        canvas.add(face)
-        canvas.paint()
-        canvas.eventloop()
     
-    except Exception, e:
-        import traceback
-        print traceback.format_exc()
-        print 'Caught Exception: '+str(e)
-        sys.exit(0)
-    
+            self.face.page.svg.text = page
+            self.face.page.refresh(svg_reload=True)
+
+            canvas.add(self.face)
+            canvas.paint()
+            canvas.eventloop()
+        
+        except Exception, e:
+            import traceback
+            print traceback.format_exc()
+            print 'Caught Exception: '+str(e)
+            sys.exit(0)
+        
 if __name__ == '__main__':
-    main()
-
+    App().main()
+    
