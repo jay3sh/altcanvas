74478302ca9901712a8ca8b8221019fa76db2b5f
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-03-28 16:26:23 +0000

    This is more suitable for inkface backend.

--- scratch/qtapp.py
+++ scratch/qtapp.py
@@ -3,18 +3,27 @@ import sys
 from PyQt4 import QtGui, QtCore
 
 
-class Canvas(QtGui.QWidget):
-    def paintEvent(self, event):
-        painter = QtGui.QPainter(self)
-        painter.drawLine(10,10,20,20)
-
 class App(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
 
-        canvas = Canvas()
+        self.scene = QtGui.QGraphicsScene()
 
-        self.setCentralWidget(canvas)
+        self.view = QtGui.QGraphicsView(self.scene)
+        self.view.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.view.setInteractive(True)
+        self.setCentralWidget(self.view)
+
+        pixmap = QtGui.QPixmap(50,50)
+        pixmap.fill(QtCore.Qt.transparent)
+        painter = QtGui.QPainter()
+        painter.begin(pixmap)
+        painter.setBrush(QtGui.QBrush(QtGui.QColor(255,255,255)))
+        painter.drawLine(10,10,20,20)
+        painter.end()
+
+        image = self.scene.addPixmap(pixmap)
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
