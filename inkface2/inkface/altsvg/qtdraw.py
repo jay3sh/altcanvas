8b4aa1e59a16014248c8bb969fcfa6da27851ceb
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-03-31 11:15:31 +0000

    Support for drawing paths in Qt.
    Updated the style get_style_object function as in the cairo version. Also some
    misc changes.

--- inkface2/inkface/altsvg/qtdraw.py
+++ inkface2/inkface/altsvg/qtdraw.py
@@ -11,11 +11,14 @@
     Rendering routines for individual nodes
 '''
 
-from inkface.altsvg.qtstyle import get_style_object
+from inkface.altsvg.qtstyle import get_style_object, parse_length
 from inkface.altsvg import TAG_RECT, TAG_PATH, \
     TAG_TEXT, TAG_TSPAN, TAG_IMAGE, TAG_HREF
 
-from PyQt4.QtGui import QPainterPath
+from PyQt4.QtGui import QPainterPath, QPainter
+
+from inkface.altsvg.path import parse_path
+from inkface.altsvg.style import parse_length
 
 def draw(painter, path, style):
     pen = style.get_pen()
@@ -25,27 +28,70 @@ def draw(painter, path, style):
     if brush is not None:
         painter.fillPath(path, brush)
 
+def draw_path(painter, node, defs, simulate=False):
+    painter.save()
+    painter.setRenderHint(QPainter.Antialiasing)
+
+    style = get_style_object(node.attrib, defs)
+    path_cmds = parse_path(node.get('d'))
+
+    path = QPainterPath()
+    for cmd,param in path_cmds:
+        if cmd is 'M':
+            x, y = param
+            path.moveTo(x, y)
+            continue
+        elif cmd is 'L':
+            x, y = param
+            path.lineTo(x, y)
+            continue
+        elif cmd is 'C':
+            x1, y1, x2, y2, x3, y3 = param
+            path.cubicTo(x1, y1, x2, y2, x3, y3)
+            continue
+        elif cmd is 'A':
+            rx, ry, xrot, laflag, swflag, x, y = param
+            #draw_arc(ctx, rx, ry, xrot, laflag, swflag, x, y)
+            continue
+        elif cmd is 'Z':
+            path.closeSubpath()
+
+    if simulate:
+        bRect = path.boundingRect()
+        ex1, ey1, ex2, ey2 = \
+            (bRect.left(), bRect.top(), bRect.right(), bRect.bottom())
+        try:
+            sw = float(style.stroke_width)
+        except AttributeError, ae:
+            sw = 0
+        extents = (ex1-sw, ey1-sw, ex2+sw, ey2+sw)
+
+        painter.restore()
+        return extents
+    else:
+        draw(painter, path, style)
+
+    painter.restore()
+
 def draw_rect(painter, node, defs, simulate=False):
     ''' Render 'rect' SVG element '''
     painter.save()
     style = None
 
-    style_str = node.attrib.get('style')
-    if style_str:
-        style = get_style_object(style_str, defs)
+    style = get_style_object(node.attrib, defs)
 
-    x = float(node.attrib.get('x'))
-    y = float(node.attrib.get('y'))
-    w = float(node.attrib.get('width'))
-    h = float(node.attrib.get('height'))
+    x = parse_length(node.attrib.get('x'))
+    y = parse_length(node.attrib.get('y'))
+    w = parse_length(node.attrib.get('width'))
+    h = parse_length(node.attrib.get('height'))
 
     rx = node.attrib.get('rx')
     ry = node.attrib.get('ry')
 
     if rx:
-        rx = float(rx)
+        rx = parse_length(float(rx))
     if ry:
-        ry = float(ry)
+        ry = parse_length(float(ry))
 
     path = QPainterPath()
 
@@ -74,6 +120,7 @@ def draw_rect(painter, node, defs, simulate=False):
 NODE_DRAW_MAP = \
     {
         TAG_RECT:draw_rect,
+        TAG_PATH:draw_path,
     }
 
 
--- inkface2/inkface/altsvg/qtstyle.py
+++ inkface2/inkface/altsvg/qtstyle.py
@@ -18,8 +18,7 @@ from PyQt4.QtGui import QColor, QPen, QBrush
 from PyQt4.QtCore import Qt
 
 style_map = {}
-
-def get_style_object(style_str, defs):
+def get_style_object(attrib, defs):
     '''
     This method creates a Style object and stores it in a dictionary
     keyed on the style_str. That way next request for parsing same
@@ -27,7 +26,24 @@ def get_style_object(style_str, defs):
     is saved.
     Repetitive parsing requests for same style string are very common
     hence this strategy helps improve performance.
+
+
+    As per SVG specs, style attributes can also be direct attributes of 
+    a node. Adobe Illustrator seems to follow that. So, if we do not get
+    a 'style' attribute in a node, we will search for individual style
+    attributes and serialize them into string. Serializing them is important
+    because that will caching commonly used styles.
     '''
+
+    if attrib.has_key('style'):
+        style_str = attrib.get('style')
+    else:
+        style_list = []
+        for att in STYLE_ATTRIBUTES:
+            if attrib.has_key(att):
+                style_list.append(att+':'+attrib[att])
+        style_str = ';'.join(style_list)
+
     if style_str in style_map:
         return style_map[style_str]
     else:
@@ -79,7 +95,7 @@ class Style:
     def __html2rgb(self, html_color):
         ''' Converts HTML color code to normalized RGB values '''
         try:
-            html_color = html_color.replace(' ','')
+            html_color = html_color.lower().replace(' ','')
             if html_color.startswith('rgb'):
                 pattern = 'rgb\(([0-9,]+)\)'
                 m = re.search(pattern, html_color)
@@ -100,10 +116,10 @@ class Style:
 
 
     def get_brush(self):
-        r = g = b = 0
-        a = 255
+        r = g = b = 0.
+        a = 255.
         if self.__style__.has_key('fill'):
-            if self.__style__['fill'] is 'none':
+            if self.__style__['fill'] == 'none':
                 return None
             elif self.__is_url(self.__style__['fill']):
                 raise Exception('Not implemented')
@@ -111,7 +127,7 @@ class Style:
                 r, g, b = self.__html2rgb(self.__style__['fill'])
 
         if self.__style__.has_key('fill-opacity'):
-            a = 255 * float(self.__style__['fill-opacity'])
+            a = 255. * float(self.__style__['fill-opacity'])
 
         brush = QBrush()
         brush.setColor(QColor(r, g, b, a))
@@ -120,9 +136,8 @@ class Style:
         return brush
             
     def get_pen(self):
-           
-        r = g = b = 0
-        a = 255
+        r = g = b = 0.
+        a = 255.
         if self.__style__.has_key('stroke'):
             if self.__style__['stroke'] == 'none':
                 return None
@@ -131,10 +146,9 @@ class Style:
             else:
                 r, g, b = self.__html2rgb(self.__style__['stroke'])
         if self.__style__.has_key('stroke-opacity'):
-            a = 255 * float(self.__style__['stroke-opacity'])
+            a = 255. * float(self.__style__['stroke-opacity'])
  
         pen = QPen()
-        print 'setting pen to '+str((r,g,b,a))
         pen.setColor(QColor(r, g, b, a))
 
         if self.__style__.has_key('stroke-width'):
