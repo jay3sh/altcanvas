2963501ecedaa01de7421188b9812a8da52c60cb
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-04-05 08:24:07 +0000

    An Evas based twitter client.

---
+++ inkface2/apps/etwt/code/setup.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+
+setup(name='etwt',
+    version='0.0.1',
+    description='SVG GUI Framework Inkface with pygame backend',
+    author='XXX',
+    author_email='XXX@XXX.com',
+    url='XXX',
+    scripts=['etwt'],
+    data_files=[],
+    packages=['etwtlib']
+)
