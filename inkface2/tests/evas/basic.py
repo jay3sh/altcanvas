d9f163a1fde4d9ba1f888ef1e2acb483d519bd1c
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-04-08 04:27:05 +0000

    fixing imports. removing now redundant statements, they are moved inside library.

--- inkface2/tests/evas/basic.py
+++ inkface2/tests/evas/basic.py
@@ -1,6 +1,6 @@
 
 import sys
-from inkface.canvas.ecanvas import ECanvas, EFace
+from inkface.evas import ECanvas, EFace
 
 try:
     face = EFace(sys.argv[1])
@@ -8,11 +8,6 @@ try:
     canvas = ECanvas((face.svg.width,face.svg.height))
 
     face.load_elements(canvas)
-
-    for el in face.elements:
-        x, y = el.get_position()
-        el.image.move(x, y)
-        el.image.show()
 
     canvas.eventloop()
 
