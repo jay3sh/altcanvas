c92a53389f767dc27589920f6ef18bb92bb62aed
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-04-10 07:27:51 +0000

    image loading related.

--- inkface2/apps/etwt/code/etwtlib/constants.py
+++ inkface2/apps/etwt/code/etwtlib/constants.py
@@ -1,9 +1,10 @@
 
 import os
 
-TWITINK_RC  = os.environ['HOME']+os.sep+'.twitinkrc'
-PREFIX      = '..'
-SVG_DIR     = os.path.join(PREFIX,'svg')
-FRAMERATE   = 30
+TWITINK_RC      = os.environ['HOME']+os.sep+'.twitinkrc'
+IMAGE_CACHE_DIR = os.path.join(os.path.sep+'tmp','.twitinkimg')
 
+PREFIX          = '..'
+SVG_DIR         = os.path.join(PREFIX,'svg')
+FRAMERATE       = 30
 
