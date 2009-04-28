d00191a965bf8ffea036da200fe30ad1ed4773de
jayeshsalvi <jayeshsalvi@5ac608b4-643a-0410-a044-37e87d4ae427>
2009-04-28 18:30:22 +0000

    This fix takes a completely different approach to create .ink file.
    Instead of using inkface library it uses inkscape itself to render individual
    components. Using --export-id-only, -X and -Y options gives all the necessary
    information needed to build .ink files.
    
    Tested the newly generated .ink files and they work in place of old ones.

--- inkplug/inkplug.py
+++ inkplug/inkplug.py
@@ -4,16 +4,15 @@ import os
 import sys
 
 sys.path.append('/usr/share/inkscape/extensions')
-sys.path.append('/home/jayesh/altcanvas/inkface2')
 
+import subprocess
 import xml.etree.ElementTree
 import zipfile
 
 import random
 random.seed()
 
-from inkface.altsvg import VectorDoc
-
+from xml.etree.ElementTree import ElementTree
 
 TMP_ZIPFILE = '/tmp/inkgui-'+str(random.randint(1,10000))+'.zip'
 
@@ -21,6 +20,37 @@ OUTPUT_DIR  = '/tmp/'
 PNG_DIR     = os.path.join(OUTPUT_DIR,str(random.randint(1,10000)))
 os.makedirs(PNG_DIR)
 
+SVG_NS      = "{http://www.w3.org/2000/svg}"
+INKSCAPE_NS = "{http://www.inkscape.org/namespaces/inkscape}"
+XLINK_NS    = "{http://www.w3.org/1999/xlink}"
+
+TAG_DEFS            = SVG_NS+'defs'
+TAG_METADATA        = SVG_NS+'metadata'
+TAG_G               = SVG_NS+'g'
+TAG_INKSCAPE_LABEL  = INKSCAPE_NS+'label'
+
+class VectorDoc:
+    ''' Class encapsulating a single SVG document '''
+    defs = {}
+    def __init__(self, svgname):
+        ''' load and parse SVG document, create ElementTree from the same '''
+
+        self.svgfilepath = svgname
+        self.tree = ElementTree()
+        self.tree.parse(svgname)
+
+    def get_elements(self):
+        for node in self.tree.getroot().getchildren():
+            if node.tag == TAG_DEFS: continue
+
+            if node.tag == TAG_METADATA: continue
+
+            if node.tag == TAG_G:
+                for cnode in node.getchildren():
+                    yield cnode
+                continue
+
+            yield node
 
 def alert(msg):
     import gtk
@@ -35,38 +65,77 @@ def alert(msg):
  
 
 def GenerateInk(svgname):
-    vDoc = VectorDoc(svgname)
-    elements = vDoc.get_elements()
 
+    vDoc = VectorDoc(svgname)
     zip = zipfile.ZipFile(TMP_ZIPFILE,'w')
-    
+
     root = xml.etree.ElementTree.Element('inkguiinfo')
 
-    for elem in elements:
-        try:
-            png_name = elem.id+'.png'
-        except AttributeError, ae:
-            # this must be background
-            png_name = 'bg.png'
-    
+    debug = open("/tmp/debug.txt","w")
+
+    for node in vDoc.get_elements():
+        id = node.get('id')
+        if id == 'base': continue
+        label = node.get(TAG_INKSCAPE_LABEL)
+        png_name = id+".png"
         png_path = os.path.join(PNG_DIR, png_name)
 
         attrib = {
             'filename'  : png_name,
-            'x'         : str(elem.x),
-            'y'         : str(elem.y),
         }
-        try:
-            attrib['label'] = elem.label
-        except AttributeError, ae:
-            # Elements that are not labeled
-            pass
+        if label:
+            attrib['label'] = label
 
+        # Generate PNG
+        command = ['/usr/bin/inkscape','--export-id-only',
+                    '-i', id, '-e', png_path, '-f', svgname]
+        of = open("/tmp/cout.txt","w")
+        ef = open("/tmp/cerr.txt","w")
+        pid = subprocess.Popen(command, stdout=of, stderr=ef)
+        pid.wait()
+        of.close()
+        ef.close()
+
+        # Find X
+        command = ['/usr/bin/inkscape','-X', '--query-id',
+                    id, '-f', svgname]
+        of = open("/tmp/cout.txt","w")
+        ef = open("/tmp/cerr.txt","w")
+        pid = subprocess.Popen(command, stdout=of, stderr=ef)
+        pid.wait()
+        of.close()
+        ef.close()
+        of = open("/tmp/cout.txt","r")
+        xvalue = of.read()
+        of.close()
+        debug.write(xvalue+"\n")
+
+        # Find Y
+        command = ['/usr/bin/inkscape','-Y', '--query-id',
+                    id, '-f', svgname]
+        of = open("/tmp/cout.txt","w")
+        ef = open("/tmp/cerr.txt","w")
+        pid = subprocess.Popen(command, stdout=of, stderr=ef)
+        pid.wait()
+        of.close()
+        ef.close()
+        of = open("/tmp/cout.txt","r")
+        yvalue = of.read()
+        of.close()
+        debug.write(yvalue+"\n")
+ 
+        attrib['x'] = xvalue
+        attrib['y'] = yvalue
+
+        debug.write(str(command)+"\n")
+
+        debug.write(str(attrib)+"\n")
         xml.etree.ElementTree.SubElement(root, tag='png', attrib=attrib)
-        elem.surface.write_to_png(png_path)
-    
+ 
+ 
         zip.write(png_path, png_name)
-    
+
+    debug.close()
 
     guiinfo_fname = os.path.join(PNG_DIR,'inkgui.xml')
     guiinfo = open(guiinfo_fname,'w')
